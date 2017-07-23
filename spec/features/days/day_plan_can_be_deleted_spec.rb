4e074c38e64a8034e1d1115ff3c504e70df9c9a5
Samantha Gongora <samantha.gongora@gmail.com>
2017-07-23 10:21:21 -0600

    consolidates meal plan tests

--- spec/features/days/day_plan_can_be_deleted_spec.rb
+++ spec/features/days/day_plan_can_be_deleted_spec.rb
@@ -1,13 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe "User visits account page" do
-  scenario "can delete a meal plan" do
+RSpec.describe "User can delete meal plan" do
+  scenario "from index page" do
     user = create(:user)
     day1 = create(:day, user: user)
     day2 = create(:day, user: user)
 
     visit user_path(user)
     click_on 'Delete Meal Plan', match: :first
+
+    expect(page).to have_content('Meal Plan Deleted!')
+    expect(page).to have_content(day2.date.strftime('%A, %B %d, %Y'))
+  end
+
+  scenario "from show page" do
+    user = create(:user)
+    day1 = create(:day, user: user)
+    day2 = create(:day, user: user)
+
+    visit user_day_path(user, day1)
+    click_on 'Delete Meal Plan'
 
     expect(page).to have_content('Meal Plan Deleted!')
     expect(page).to have_content(day2.date.strftime('%A, %B %d, %Y'))
--- spec/features/days/day_plan_can_be_updated_spec.rb
+++ spec/features/days/day_plan_can_be_updated_spec.rb
@@ -1,13 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe "User visits account page" do
-  scenario "can update a meal plan" do
+RSpec.describe "Meal plan can be updated" do
+  scenario "from index page" do
     user = build(:user)
     recipes = build_list(:recipe, 4)
     day = create(:day, user: user, recipes: recipes)
     new_recipe = create(:recipe)
 
     visit user_path(user)
+    click_on 'Edit Meal Plan'
+
+    select new_recipe.name, from: "day_form_breakfast_id"
+
+    click_on 'Update Meal Plan'
+
+    expect(page).to have_content('Meal Plan Updated!')
+    expect(page).to have_content(new_recipe.name)
+  end
+
+  scenario "from show page" do
+    user = build(:user)
+    recipes = build_list(:recipe, 4)
+    day = create(:day, user: user, recipes: recipes)
+    new_recipe = create(:recipe)
+
+    visit user_path(user)
+    click_on day.date.strftime('%A, %B %d, %Y')
     click_on 'Edit Meal Plan'
 
     select new_recipe.name, from: "day_form_breakfast_id"
