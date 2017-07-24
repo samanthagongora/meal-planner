f78ff1c73f3b135aa1dcc1e7a869488d6462bc6a
Samantha Gongora <samantha.gongora@gmail.com>
2017-07-23 21:21:57 -0600

    adds calories per serving and total calories to views

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,8 @@ class Recipe < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :calories, presence: true, numericality: true
   validates :servings, presence: true, numericality: true
+
+  def calories_per_serving
+    calories/servings
+  end
 end
